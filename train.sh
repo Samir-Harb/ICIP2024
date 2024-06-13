bade39e0b12119335f428854d2569af7e8687834
Samir-Farag <119358330+Samir-Farag@users.noreply.github.com>
2024-06-13 17:07:07 -0400

    Delete train.sh

--- train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-#python /home/assale02/LandmarkDetector/test.py > /home/assale02/LandmarkDetector/log.log
-echo "$1, $2"
-python train.py "$1" "$2" > ./log/log"$1"_"$2".txt 2>&1
-# python train.py "$1" "$2" > log"$1"_"$2".txt 
-
-#python /home/assale02/LandmarkDetector/test_complete_train.py
